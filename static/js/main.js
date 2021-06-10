4e8a6eeca1134265471e71537669ad6c21083e37
aga <aga3134@gmail.com>
2021-06-10 20:00:06 +0800

    add ui layout

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,12 @@
 var g_APP = new Vue({
     el: "#app",
-    data: {},
+    data: {
+        map: null,
+        openOptionPanel: false,
+        openQuestPanel: false,
+        openChartPanel: false,
+        openAboutPanel: false
+    },
     delimiters: ['[[',']]'],    //vue跟jinja的語法會衝突
     created: function(){
         window.addEventListener('load', function() {
@@ -12,13 +18,47 @@ var g_APP = new Vue({
         InitMap: function(){
             var accessToken = $("meta[name='accessToken']").attr("content");
             mapboxgl.accessToken = accessToken;
-            var map = new mapboxgl.Map({
+            this.map = new mapboxgl.Map({
                 container: 'map',
-                style: 'mapbox://styles/mapbox/satellite-v9',
-                center: [121, 24.82],
-                pitch: 45,
-                zoom: 14
+                style: 'mapbox://styles/aga3134/ckpqd8kxb0pph17px4usqsea5',
+                center: [121, 23.7],
+                zoom: 6.5
             });
+            
+            this.map.on('load', function(){
+                this.map.addSource('mapbox-dem', {
+                    'type': 'raster-dem',
+                    'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+                    'tileSize': 512,
+                    'maxzoom': 13
+                });
+                this.map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+                this.map.addLayer({
+                    'id': 'sky',
+                    'type': 'sky',
+                    'paint': {
+                        'sky-type': 'atmosphere',
+                        'sky-atmosphere-sun': [0.0, 0.0],
+                        'sky-atmosphere-sun-intensity': 15
+                    }
+                });
+                this.map.flyTo({
+                    center: [121, 24.82],
+                    pitch: 45,
+                    zoom: 14,
+                    bearing: 0,
+                    speed: 0.5,
+                    essential: true
+                });
+                
+                //add search box
+                var geocoder = new MapboxGeocoder({
+                    accessToken: mapboxgl.accessToken,
+                    placeholder: "搜尋地點",
+                    mapboxgl: mapboxgl
+                });
+                document.getElementById('geocoder').appendChild(geocoder.onAdd(this.map));
+            }.bind(this));
         }
     }
 });

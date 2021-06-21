6229da80e4c9ea05e16106a25c2a7fa403864f31
aga <aga3134@gmail.com>
2021-06-21 11:30:02 +0800

    use promise to handle async data loading

--- static/js/main.js
+++ static/js/main.js
@@ -7,8 +7,15 @@ var g_APP = new Vue({
         openChartPanel: false,
         openAboutPanel: false,
         questArr: [],
-        traceRiver:{
-            river: [],
+        curQuest: {
+            quest: null,
+            source: {},
+            layer: {},
+            chart: {},
+            option: {},
+        },
+        tracePath:{
+            originPath: [],
             displayPath: [],
             curIndex: 0,
             timer: null
@@ -17,15 +24,13 @@ var g_APP = new Vue({
     delimiters: ['[[',']]'],    //vue跟jinja的語法會衝突
     created: function(){
         window.addEventListener('load', function() {
-            this.InitMap();
+            this.InitMap(function(){
+                this.LoadQuest();
+            }.bind(this));
         }.bind(this));
-        
-        this.questArr = [
-            {"quest": "水從頭前溪源頭流到海洋要多久?"}
-        ];
     },
     methods:{
-        InitMap: function(){
+        InitMap: function(callback){
             var accessToken = $("meta[name='accessToken']").attr("content");
             mapboxgl.accessToken = accessToken;
             this.map = new mapboxgl.Map({
@@ -69,37 +74,54 @@ var g_APP = new Vue({
                 });
                 document.getElementById('geocoder').appendChild(geocoder.onAdd(this.map));
 
-                //load geojson data
-                $.get("topo/river", function(data){
-                    this.traceRiver.river = $.extend(true, {}, data);;
-                    this.traceRiver.displayPath = data;
-                    //console.log(data);
-                    this.map.addSource('route', {
-                        'type': 'geojson',
-                        'data': this.traceRiver.displayPath
-                    });
-                    this.map.addLayer({
-                        'id': 'route',
-                        'type': 'line',
-                        'source': 'route',
-                        'layout': {
-                            'line-join': 'round',
-                            'line-cap': 'round'
-                        },
-                        'paint': {
-                            'line-color': '#f33',
-                            'line-width': 4
-                        }
-                    });
-                }.bind(this));
+                if(callback) callback();
             }.bind(this));
         },
+        LoadQuest: function(){
+            this.questArr = [
+                {
+                    "quest": "頭前溪長怎麼?",
+                    "geom": [
+                        {
+                            "title": "頭前溪",
+                            "type": "line",
+                            "url": "topo/river",
+                            "paint": {
+                                "line-color": "#f33",
+                                "line-width": 4
+                            }
+                        }
+                    ],
+                    "chart": [],
+                    "option": [],
+                    "action":{}
+                },
+                {
+                    "quest": "水從頭前溪源頭流到海洋走什麼路徑?",
+                    "geom": [
+                        {
+                            "title": "頭前溪路徑",
+                            "type": "line",
+                            "url": "topo/river",
+                            "paint": {
+                                "line-color": "#33f",
+                                "line-width": 4
+                            }
+                        }
+                    ],
+                    "chart": [],
+                    "option": [],
+                    "action":{
+                        "func": "TracePath",
+                        "param":{
+                            "index": 0
+                        }
+                    }
+                }
+            ];
+            this.SelectQuest(0);
+        },
         SetMapPadding: function(padding){
-            var p = this.map.getPadding();
-            if("left" in padding) p.left = padding.left;
-            if("right" in padding) p.right = padding.right;
-            if("top" in padding) p.top = padding.top;
-            if("bottom" in padding) p.bottom = padding.bottom; 
             this.map.easeTo({padding: padding, duration: 1000});
         },
         OpenOptionPanel: function(){
@@ -132,37 +154,116 @@ var g_APP = new Vue({
         CloseAboutPanel: function(){
             this.openAboutPanel = false;
         },
-        TraceRiver: function(){
+        ClearQuest: function(){
+            for(var key in this.curQuest.layer){
+                this.map.removeLayer(this.curQuest.layer[key].name);
+            }
+            this.curQuest.layer = {};
+
+            for(var key in this.curQuest.source){
+                this.map.removeSource(this.curQuest.source[key].name);
+            }
+            this.curQuest.source = {};
+
+            //clear charts
+
+            //clear option UI
+            
+            //clear action
+            if(this.tracePath.timer){
+                window.clearInterval(this.tracePath.timer);
+                this.tracePath.timer = null;
+            }
+        },
+        SelectQuest: function(i){
+            this.ClearQuest();
+
+            var q = this.questArr[i];
+            this.curQuest.quest = q.quest;
+
+            var promiseArr = [];
+            for(let i=0;i<q.geom.length;i++){
+                let geom = q.geom[i];
+                promiseArr.push(new Promise((resolve,reject) => {
+                    $.get(geom.url, (data) => {
+                        var key = this.ToHash(q.quest)+"_"+i;
+                        //console.log(key);
+                        this.map.addSource(key, {
+                            "type": "geojson",
+                            "data": data
+                        });
+                        this.map.addLayer({
+                            "id": key,
+                            "type": geom.type,
+                            "source": key,
+                            "paint": geom.paint
+                        });
+                        this.curQuest.source[key] = {"name":key, "data":data};
+                        this.curQuest.layer[key] = {"name":key};
+                        resolve(key);
+                    });
+                }));
+            }
+
+            //add charts
+
+            //add option UI
+
+            //call action
+            Promise.all(promiseArr).then((keyArr) => {
+                //console.log(keyArr);
+                if(q.action.func in this) this[q.action.func](q.action.param);
+            });
+
+        },
+        TracePath: function(param){
+            var key = this.ToHash(this.curQuest.quest)+"_"+param.index;
+            
             var ClearTimer = function(){
-                window.clearInterval(this.traceRiver.timer);
-                this.traceRiver.timer = null;
-                this.map.getSource("route").setData(this.traceRiver.river);
+                window.clearInterval(this.tracePath.timer);
+                this.tracePath.timer = null;
+                this.tracePath.displayPath.coordinates[0] = [];
+                this.map.getSource(key).setData(this.tracePath.displayPath);
                 this.map.easeTo({pitch: 30});
             }.bind(this);
 
-            if(this.traceRiver.timer){
+            if(this.tracePath.timer){
                 ClearTimer();
             }
             else{
-                var coord = this.traceRiver.river.coordinates[0];
-                this.traceRiver.displayPath.coordinates[0] = [coord[0]];
-                this.traceRiver.curIndex = 0;
-                this.map.flyTo({ "center": coord[0], "zoom": 14, "pitch":35});
+                var source = this.curQuest.source[key];
+                this.tracePath.originPath = source.data;
+                this.tracePath.displayPath = $.extend(true, {}, source.data);
+                var coord = this.tracePath.originPath.coordinates[0];
+                this.tracePath.displayPath.coordinates[0] = [coord[0]];
+                this.tracePath.curIndex = 0;
+                var source = this.map.getSource(key);
+                if(source) source.setData(this.tracePath.displayPath);
+                
+                this.map.flyTo({"center": coord[0], "zoom": 14, "pitch":35});
                 this.map.once("moveend", function(){
-                    this.traceRiver.timer = window.setInterval(function(){
-                        if(this.traceRiver.curIndex < coord.length){
-                            this.traceRiver.displayPath.coordinates[0].push(coord[this.traceRiver.curIndex]);
-                            this.map.getSource("route").setData(this.traceRiver.displayPath);
-                            this.map.panTo(coord[this.traceRiver.curIndex]);
-                            this.traceRiver.curIndex++;
+                    this.tracePath.timer = window.setInterval(function(){
+                        if(this.tracePath.curIndex < coord.length){
+                            this.tracePath.displayPath.coordinates[0].push(coord[this.tracePath.curIndex]);
+                            var source = this.map.getSource(key);
+                            if(source) source.setData(this.tracePath.displayPath);
+                            this.map.panTo(coord[this.tracePath.curIndex]);
+                            this.tracePath.curIndex++;
                         }
                         else ClearTimer();
                     }.bind(this), 100);
                 }.bind(this));
-
             }
-            
-
-        }
+        },
+        ToHash: function(str){
+            var hash = 0, i, chr;
+            if (str.length === 0) return hash;
+            for (i = 0; i < str.length; i++) {
+                chr   = str.charCodeAt(i);
+                hash  = ((hash << 5) - hash) + chr;
+                hash |= 0; // Convert to 32bit integer
+            }
+            return hash;
+        },
     }
 });
